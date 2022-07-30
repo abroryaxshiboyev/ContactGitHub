6872e99c372b17032c9de7cb9874fa3f352fafae
iztileuoff <bektemir0714@gmail.com>
2022-07-30 10:37:10 +0500

    blade

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,7 +8,36 @@
 </head>
 <body>
     
-    
+    <table border="1">
+        <tr>
+            <td>№</td>
+            <td>title</td>
+            <td>description</td>
+            <td>name</td>
+            <td>email</td>
+            <td>file</td>
+            <td>delete</td>
+        </tr>
+
+        @foreach ($messages as $message)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $message->title }}</td>
+            <td>{{ $message->description }}</td>
+            <td>{{ $message->user->name }}</td>
+            <td>{{ $message->user->email }}</td>
+            <td>{{ $message->file }}</td>
+            <td>
+                <form action="{{ url('admins/'.$message->id ) }}" method="post">
+                    @csrf
+                    <input name="_method" type="hidden" value="DELETE">
+                    <input type="submit" value="DELETE">
+                </form>
+            </td>
+        </tr>
+        @endforeach
+
+    </table>
 
 </body>
 </html>
