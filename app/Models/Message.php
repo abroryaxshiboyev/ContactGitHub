4e64dfe620d699690d6069496439a995e4ec7bb5
iztileuoff <bektemir0714@gmail.com>
2022-07-30 11:50:00 +0500

    guarded ham belongsTo

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -9,6 +9,10 @@ class Message extends Model
 {
     use HasFactory;
 
+    protected $guarded = [
+        'id'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
